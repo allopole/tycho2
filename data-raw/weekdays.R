4adda2cb4afca49b9e9015782b50eb0fc329fcca
Eric Marty <allopole@noreply.github.com>
2018-08-16 15:24:05 -0400

    replace weekdays.rda with sysdata.rda

---
+++ data-raw/weekdays.R
@@ -0,0 +1,7 @@
+# data object "weekdays" (stored with package):
+weekdays <- c(
+  "Sunday"=1,"Monday"=2,"Tuesday"=3,"Wednesday"=4,"Thursday"=5,"Friday"=6,"Saturday"=7,
+  "isoweek.start"=2,"isoweek.end"=1,
+  "cdcweek.start"=1,"cdcweek.end"=7
+)
+devtools::use_data(weekdays, internal = TRUE)
